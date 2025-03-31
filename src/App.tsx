dfb7a0e0cc61fa5b3847eb7112c9fd8b3e3adc1e
kekaze <jroyceyanson@gmail.com>
2025-03-31 10:11:14 +0800

    :package: removed unnecessary package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import { Toaster } from 'sonner'
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
@@ -10,10 +9,7 @@ import Preferences from "./pages/Preferences";
 import Results from "./pages/Results";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
 const App = () => (
-  <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster richColors />
       <BrowserRouter>
@@ -28,7 +24,6 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
-  </QueryClientProvider>
 );
 
 export default App;
